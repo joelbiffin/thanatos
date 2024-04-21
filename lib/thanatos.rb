3b22c3a3faf4cc6f99b1f640985d4dbf88611dd5
Joel Biffin <joelbiffin@gmail.com>
2024-04-21 13:01:04 +0100

    Push method definitions and calls under constants
    This refactor allows us to define method calls and method definitions
    on the scope that calls them.
    
    This might be a trap, but feels like the right thing to do. For now, we
    maintain the method_definitions and method_calls public api.

--- lib/thanatos.rb
+++ lib/thanatos.rb
@@ -1,7 +1,9 @@
 require 'prism'
 
 class Thanatos
-  attr_reader :ast, :method_definitions, :method_calls, :constants
+  attr_reader :ast, :constants
+
+  Methods = Data.define(:definitions, :calls)
 
   def initialize(path: 'example/single_class.rb')
     @ast = Prism.parse_file(path)
@@ -14,18 +16,32 @@ class Thanatos
     traverse_prism(ast.value)
   end
 
+  def method_definitions
+    constants.map do |constants, methods|
+      methods.definitions
+    end.flatten.compact
+  end
+
+  def method_calls
+    constants.map do |constants, methods|
+      methods.calls
+    end.flatten.compact
+  end
+
   def traverse_prism(node, scope: [])
     case node
     when Prism::ClassNode, Prism::ModuleNode
       namespace_scope = scope + [node.name]
-      constants[namespace_scope.join("::")] = true
+      constants[namespace_scope.join("::")] = Methods.new(definitions: [], calls: [])
       traverse_children(node, scope: namespace_scope) 
       
       return
     when Prism::DefNode
-      method_definitions << node.name
+      @method_definitions << node.name
+      constants[scope.join("::")].definitions << node.name
     when Prism::CallNode 
-      method_calls << node.name
+      @method_calls << node.name
+      constants[scope.join("::")].calls << node.name
     end
 
     traverse_children(node, scope:)
