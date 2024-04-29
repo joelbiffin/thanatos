2e523120968dc49218a9dc28b87a80622d3a5d95
Joel Biffin <joel@meetcleo.com>
2024-04-29 21:03:45 +0100

    Extend hash to differentiate methods by visibility
    Contants#methods is now a hash rather than an array, with a structure
    of,
    
    ```ruby
    {
      methods: {
        private: [...],
        protected: [...],
        public: [...],
      }
    }
    ```

---
+++ example/class_with_multiple_method_visibilities.rb
@@ -0,0 +1,17 @@
+class Foo
+  def public_thing
+    "protected thing"
+  end
+
+  protected
+
+  def protected_thing
+    "protected thing"
+  end
+
+  private
+
+  def private_thing
+    "do something else"
+  end
+end
